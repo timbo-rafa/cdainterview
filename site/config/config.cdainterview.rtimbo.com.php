f495d7162403ae4fa55d2b342906e1b6c457d349
Rafael Timbó <timbo.rafa@gmail.com>
2020-05-07 21:33:59 -0300

    production config.php

---
+++ site/config/config.cdainterview.rtimbo.com.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'debug' => false,
+    'routes' => [
+        [
+            'pattern' => 'logout',
+            'action'  => function() {
+                
+                if ($user = kirby()->user()) {
+                    $user->logout();
+                }
+
+                go('login');
+            }
+        ]
+    ],
+    'email' => [
+        'transport' => [
+          'type' => 'smtp',
+          'host' => 'smtp.gmail.com',
+          'port' => 587,
+          'security' => true,
+          'auth' => true,
+          'username' => 'timbo.software+kirby.cdainterview@gmail.com',
+          'password' => 'tdtihvichqjhzdsr', #getenv('GMAIL_SMTP_PASSWORD'),
+        ]
+    ]
+];
