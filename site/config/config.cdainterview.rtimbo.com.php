ac62011881e43229f032ddb1cd44fd5bd8a0c071
Rafael Timbó <timbo.rafa@gmail.com>
2020-05-07 23:23:29 -0300

    gmail smtp

--- site/config/config.cdainterview.rtimbo.com.php
+++ site/config/config.cdainterview.rtimbo.com.php
@@ -18,15 +18,15 @@ return [
     'panel' => [
         'install' => true
     ],
-    // 'email' => [
-    //     'transport' => [
-    //       'type' => 'smtp',
-    //       'host' => 'smtp.gmail.com',
-    //       'port' => 587,
-    //       'security' => true,
-    //       'auth' => true,
-    //       'username' => 'timbo.software+kirby.cdainterview@gmail.com',
-    //       'password' => 'tdtihvichqjhzdsr', #getenv('GMAIL_SMTP_PASSWORD'),
-    //     ]
-    // ]
+    'email' => [
+        'transport' => [
+          'type' => 'smtp',
+          'host' => 'smtp.gmail.com',
+          'port' => 587,
+          'security' => true,
+          'auth' => true,
+          'username' => 'timbo.software+kirby.cdainterview@gmail.com',
+          'password' => 'tdtihvichqjhzdsr', #getenv('GMAIL_SMTP_PASSWORD'),
+        ]
+    ]
 ];
