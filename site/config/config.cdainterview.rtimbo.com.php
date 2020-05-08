d90462363e9330df27547bdd535b95025ddeee58
Rafael Timbó <timbo.rafa@gmail.com>
2020-05-07 23:56:30 -0300

    gmail smtp

--- site/config/config.cdainterview.rtimbo.com.php
+++ site/config/config.cdainterview.rtimbo.com.php
@@ -19,27 +19,27 @@ return [
         'install' => true
     ],
     // MailGun
+    // 'email' => [
+	// 	'transport' => [
+	// 		'type' => 'smtp',
+	// 		'port' => getenv('MAILGUN_SMTP_PORT'),
+	// 		'security' => 'ssl',
+	// 		'host' => getenv('MAILGUN_SMTP_SERVER'), #'smtp.mailgun.org',
+	// 		'auth' => true,
+	// 		'username' => getenv('MAILGUN_SMTP_LOGIN'),
+	// 		'password' => getenv('MAILGUN_SMTP_PASSWORD'),
+    //     ]
+    // ],
+    //Gmail SMTP
     'email' => [
-		'transport' => [
-			'type' => 'smtp',
-			'port' => getenv('MAILGUN_SMTP_PORT'),
-			'security' => 'ssl',
-			'host' => getenv('MAILGUN_SMTP_SERVER'), #'smtp.mailgun.org',
-			'auth' => true,
-			'username' => getenv('MAILGUN_SMTP_LOGIN'),
-			'password' => getenv('MAILGUN_SMTP_PASSWORD'),
+        'transport' => [
+          'type' => 'smtp',
+          'host' => 'smtp.gmail.com',
+          'port' => 587,
+          'security' => 'tls',
+          'auth' => true,
+          'username' => 'timbo.software+kirby.cdainterview@gmail.com',
+          'password' => 'tdtihvichqjhzdsr', #getenv('GMAIL_SMTP_PASSWORD'),
         ]
     ]
-    // Gmail SMTP
-    // 'email' => [
-    //     'transport' => [
-    //       'type' => 'smtp',
-    //       'host' => 'smtp.gmail.com',
-    //       'port' => 587,
-    //       'security' => true,
-    //       'auth' => true,
-    //       'username' => 'timbo.software+kirby.cdainterview@gmail.com',
-    //       'password' => 'tdtihvichqjhzdsr', #getenv('GMAIL_SMTP_PASSWORD'),
-    //     ]
-    // ]
 ];
