184d48202d67b086a74ab5e777d560bb2710667c
Rafael Timbó <timbo.rafa@gmail.com>
2020-05-07 23:37:25 -0300

    config for herokuapp

---
+++ site/config/config.cdainterview-rtimbo.herokuapp.com.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'debug' => false,
+    'routes' => [
+        [
+            'pattern' => 'logout',
+            'action'  => function() {
+                
+                if ($user = kirby()->user()) {
+                    $user->logout();
+                }
+
+                go('login');
+            }
+        ]
+    ],
+    'panel' => [
+        'install' => true
+    ],
+    // MailGun
+    'email' => [
+		'transport' => [
+			'type' => 'smtp',
+			'port' => getenv('MAILGUN_SMTP_PORT'),
+			'security' => 'tls',
+			'host' => getenv('MAILGUN_SMTP_SERVER'), #'smtp.mailgun.org',
+			'auth' => true,
+			'username' => getenv('MAILGUN_SMTP_LOGIN'),
+			'password' => getenv('MAILGUN_SMTP_PASSWORD'),
+    ],
+];
--- site/config/config.php
+++ site/config/config.php
@@ -23,15 +23,4 @@ return [
             }
         ]
     ]
-    // 'email' => [
-    //     'transport' => [
-    //       'type' => 'smtp',
-    //       'host' => 'smtp.gmail.com',
-    //       'port' => 587,
-    //       'security' => true,
-    //       'auth' => true,
-    //       'username' => 'timbo.software+kirby.cdainterview@gmail.com',
-    //       'password' => 'tdtihvichqjhzdsr', #getenv('GMAIL_SMTP_PASSWORD'),
-    //     ]
-    // ]
 ];
